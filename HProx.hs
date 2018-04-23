63eebbb39971a7929c2842adfc86c41f8d279f84
Bin Jin <bjin@ctrl-d.org>
2018-04-23 18:23:43 +0800

    initial commit

---
+++ HProx.hs
@@ -0,0 +1,215 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module HProx
+  ( ProxySettings(..)
+  , httpProxy
+  , httpGetProxy
+  , httpConnectProxy
+  , forceSSL
+  ) where
+
+import           Control.Applicative       ((<|>))
+import           Control.Concurrent.Async  (concurrently)
+import           Control.Exception         (SomeException, try)
+import           Control.Monad             (unless, void, when)
+import           Control.Monad.IO.Class    (liftIO)
+import qualified Data.Binary.Builder       as BB
+import qualified Data.ByteString           as BS
+import           Data.ByteString.Base64    (decodeLenient)
+import qualified Data.ByteString.Char8     as BS8
+import qualified Data.CaseInsensitive      as CI
+import qualified Data.Conduit.Network      as CN
+import           Data.Maybe                (fromJust, fromMaybe, isJust,
+                                            isNothing)
+import qualified Network.HTTP.Client       as HC
+import           Network.HTTP.ReverseProxy (ProxyDest (..), SetIpHeader (..),
+                                            WaiProxyResponse (..), def,
+                                            waiProxyToSettings, wpsSetIpHeader)
+import qualified Network.HTTP.Types        as HT
+import qualified Network.HTTP.Types.Header as HT
+
+import           Data.Conduit
+import           Network.Wai
+
+data ProxySettings = ProxySettings
+  { proxyAuth  :: Maybe (BS.ByteString -> Bool)
+  , passPrompt :: Maybe BS.ByteString
+  }
+
+httpProxy :: ProxySettings -> HC.Manager -> Middleware
+httpProxy set mgr = httpGetProxy set mgr . httpConnectProxy set
+
+forceSSL :: Middleware
+forceSSL app req respond
+    | isSecure req = app req respond
+    | otherwise    = redirectToSSL req respond
+
+redirectToSSL :: Application
+redirectToSSL req respond
+    | Just host <- requestHeaderHost req = respond $ responseLBS
+        HT.status301
+        [("Location", "https://" `BS.append` host)]
+        ""
+    | otherwise                          = respond $ responseLBS
+        (HT.mkStatus 426 "Upgrade Required")
+        [("Upgrade", "TLS/1.0, HTTP/1.1"), ("Connection", "Upgrade")]
+        ""
+
+parseHostPort :: BS.ByteString -> Maybe (BS.ByteString, Int)
+parseHostPort hostPort = do
+    lastColon <- BS8.elemIndexEnd ':' hostPort
+    port <- BS8.readInt (BS.drop (lastColon+1) hostPort) >>= checkPort
+    return (BS.take lastColon hostPort, port)
+  where
+    checkPort (p, bs)
+        | BS.null bs && 1 <= p && p <= 65535 = Just p
+        | otherwise                          = Nothing
+
+parseHostPortWithDefault :: Int -> BS.ByteString -> (BS.ByteString, Int)
+parseHostPortWithDefault defaultPort hostPort =
+    fromMaybe (hostPort, defaultPort) $ parseHostPort hostPort
+
+isProxyHeader :: HT.HeaderName -> Bool
+isProxyHeader k
+    | BS.length bs <= 4     = False
+    | c0 /= 112 && c0 /= 80 = False -- 'p'
+    | c1 /= 114 && c1 /= 82 = False -- 'r'
+    | c2 /= 111 && c2 /= 79 = False -- 'o'
+    | c3 /= 120 && c3 /= 88 = False -- 'x'
+    | c4 /= 121 && c4 /= 89 = False -- 'y'
+    | otherwise             = True
+  where
+    bs = CI.original k
+    idx = BS.index bs
+
+    c0 = idx 0
+    c1 = idx 1
+    c2 = idx 2
+    c3 = idx 3
+    c4 = idx 4
+
+isForwardedHeader :: HT.HeaderName -> Bool
+isForwardedHeader k
+    | BS.length bs <= 10    = False
+    | c0 /= 120 && c0 /= 88 = False -- 'x'
+    | c1 /= 45              = False -- '-'
+    | c2 /= 102 && c2 /= 70 = False -- 'f'
+    | c3 /= 111 && c3 /= 79 = False -- 'o'
+    | c4 /= 114 && c4 /= 82 = False -- 'r'
+    | c5 /= 119 && c5 /= 87 = False -- 'w'
+    | c6 /= 97  && c6 /= 65 = False -- 'a'
+    | c7 /= 114 && c7 /= 82 = False -- 'r'
+    | c8 /= 100 && c8 /= 68 = False -- 'd'
+    | c9 /= 101 && c9 /= 69 = False -- 'e'
+    | ca /= 100 && ca /= 68 = False -- 'd'
+    | otherwise             = True
+  where
+    bs = CI.original k
+    idx = BS.index bs
+
+    c0 = idx 0
+    c1 = idx 1
+    c2 = idx 2
+    c3 = idx 3
+    c4 = idx 4
+    c5 = idx 5
+    c6 = idx 6
+    c7 = idx 7
+    c8 = idx 8
+    c9 = idx 9
+    ca = idx 10
+
+isToStripHeader :: HT.HeaderName -> Bool
+isToStripHeader h = isProxyHeader h || isForwardedHeader h || h == "X-Real-IP"
+
+checkAuth :: ProxySettings -> Request -> Bool
+checkAuth pset req
+    | isNothing pauth   = True
+    | isNothing authRsp = False
+    | otherwise         = fromJust pauth decodedRsp
+  where
+    pauth = proxyAuth pset
+    authRsp = lookup HT.hProxyAuthorization (requestHeaders req)
+
+    decodedRsp = decodeLenient $ snd $ BS8.spanEnd (/=' ') $ fromJust authRsp
+
+proxyAuthRequiredResponse :: ProxySettings -> Response
+proxyAuthRequiredResponse pset = responseLBS
+    HT.status407
+    [(HT.hProxyAuthenticate, "Basic realm=\"" `BS.append` prompt `BS.append` "\"")]
+    ""
+  where
+    prompt = fromMaybe "hprox" (passPrompt pset)
+
+httpGetProxy :: ProxySettings -> HC.Manager -> Middleware
+httpGetProxy pset mgr fallback = waiProxyToSettings (return.proxyResponseFor) settings mgr
+  where
+    settings = def { wpsSetIpHeader = SIHNone }
+
+    proxyResponseFor req
+        | not isGetProxy     = WPRApplication fallback
+        | checkAuth pset req = WPRModifiedRequest nreq (ProxyDest host port)
+        | otherwise          = WPRResponse (proxyAuthRequiredResponse pset)
+      where
+        notCONNECT = requestMethod req /= "CONNECT"
+        rawPath = rawPathInfo req
+        rawPathPrefix = "http://"
+        defaultPort = 80
+        hostHeader = parseHostPortWithDefault defaultPort <$> requestHeaderHost req
+
+        isRawPathProxy = rawPathPrefix `BS.isPrefixOf` rawPath
+        hasProxyHeader = any (isProxyHeader.fst) (requestHeaders req)
+
+        isGetProxy = notCONNECT && (isRawPathProxy || isJust hostHeader && hasProxyHeader)
+
+        nreq = req
+          { rawPathInfo = newRawPath
+          , requestHeaders = filter (not.isToStripHeader.fst) $ requestHeaders req
+          }
+
+        ((host, port), newRawPath)
+            | isRawPathProxy  = (parseHostPortWithDefault defaultPort hostPortP, newRawPathP)
+            | otherwise       = (fromJust hostHeader, rawPath)
+          where
+            (hostPortP, newRawPathP) = BS8.span (/='/') $
+                BS.drop (BS.length rawPathPrefix) rawPath
+
+httpConnectProxy :: ProxySettings -> Middleware
+httpConnectProxy pset fallback req respond
+    | not isConnectProxy = fallback req respond
+    | checkAuth pset req = respond response
+    | otherwise          = respond (proxyAuthRequiredResponse pset)
+  where
+    hostPort' = parseHostPort (rawPathInfo req) <|> (requestHeaderHost req >>= parseHostPort)
+    isConnectProxy = requestMethod req == "CONNECT" && isJust hostPort'
+
+    Just (host, port) = hostPort'
+    settings = CN.clientSettings port host
+
+    backup = responseLBS HT.status500 [("Content-Type", "text/plain")]
+        "HTTP CONNECT tunneling detected, but server does not support responseRaw"
+
+    tryAndCatchAll :: IO a -> IO (Either SomeException a)
+    tryAndCatchAll = try
+
+    response
+        | HT.httpMajor (httpVersion req) < 2 = responseRaw (handleConnect True) backup
+        | otherwise                          = responseStream HT.status200 [] streaming
+      where
+        streaming write flush = do
+            flush
+            handleConnect False (requestBody req) (\bs -> write (BB.fromByteString bs) >> flush)
+
+    handleConnect :: Bool -> IO BS.ByteString -> (BS.ByteString -> IO ()) -> IO ()
+    handleConnect http1 fromClient' toClient' = CN.runTCPClient settings $ \server ->
+        let toServer = CN.appSink server
+            fromServer = CN.appSource server
+            fromClient = do
+                bs <- liftIO fromClient'
+                unless (BS.null bs) (yield bs >> fromClient)
+            toClient = awaitForever (liftIO . toClient')
+        in do
+            when http1 $ runConduit $ yield "HTTP/1.1 200 OK\r\n\r\n" .| toClient
+            void $ tryAndCatchAll $ concurrently
+                (runConduit (fromClient .| toServer))
+                (runConduit (fromServer .| toClient))
---
+++ Main.hs
@@ -0,0 +1,145 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import qualified Data.ByteString.Char8       as BS8
+import           Data.String                 (fromString)
+import           Network                     (PortID (..), listenOn)
+import qualified Network.HTTP.Client         as HC
+import           Network.TLS                 as TLS
+import           Network.Wai.Handler.Warp    (HostPreference, defaultSettings,
+                                              runSettings, runSettingsSocket,
+                                              setBeforeMainLoop, setHost,
+                                              setNoParsePath, setPort,
+                                              setServerName)
+import           Network.Wai.Handler.WarpTLS (OnInsecure (..), onInsecure,
+                                              runTLS, runTLSSocket,
+                                              tlsServerHooks, tlsSettings)
+import           Network.Wai.Middleware.Gzip (def, gzip)
+import           System.Posix.User           (UserEntry (..),
+                                              getUserEntryForName, setUserID)
+
+import           Data.Maybe
+import           Data.Monoid                 ((<>))
+import           Options.Applicative
+
+import           DumbApp                     (dumbApp)
+import           HProx                       (ProxySettings (..), forceSSL,
+                                              httpProxy)
+
+data Opts = Opts
+  { _bind :: Maybe HostPreference
+  , _port :: Int
+  , _ssl  :: [(String, CertFile)]
+  , _user :: Maybe String
+  , _auth :: Maybe FilePath
+  }
+
+data CertFile = CertFile
+  { certfile :: FilePath
+  , keyfile  :: FilePath
+  }
+
+readCert :: CertFile -> IO TLS.Credential
+readCert (CertFile c k) = either error id <$> TLS.credentialLoadX509 c k
+
+splitBy :: Eq a => a -> [a] -> [[a]]
+splitBy _ [] = [[]]
+splitBy c (x:xs)
+  | c == x    = [] : splitBy c xs
+  | otherwise = let y:ys = splitBy c xs in (x:y):ys
+
+parser :: ParserInfo Opts
+parser = info (helper <*> opts) fullDesc
+  where
+    parseSSL s = case splitBy ':' s of
+        [host, cert, key] -> Right (host, CertFile cert key)
+        _                 -> Left "invalid format for ssl certificates"
+
+    opts = Opts <$> bind
+                <*> (fromMaybe 3000 <$> port)
+                <*> ssl
+                <*> user
+                <*> auth
+
+    bind = optional $ fromString <$> strOption
+        ( long "bind"
+       <> short 'b'
+       <> metavar "bind_ip"
+       <> help "The address to bind on (default: all interfaces)")
+
+    port = optional $ option auto
+        ( long "port"
+       <> short 'p'
+       <> metavar "port"
+       <> help "port number (default 3000)")
+
+    ssl = many $ option (eitherReader parseSSL)
+        ( long "ssl"
+       <> short 's'
+       <> metavar "hostname:cerfile:keyfile"
+       <> help "enable SSL and specify a SSL certificates")
+
+    user = optional $ strOption
+        ( long "user"
+       <> short 'u'
+       <> metavar "nobody"
+       <> help "setuid after binding port")
+
+    auth = optional $ strOption
+        ( long "auth"
+       <> short 'a'
+       <> metavar "users.txt"
+       <> help "password file for proxy authentication")
+
+
+setuid :: String -> IO ()
+setuid user = getUserEntryForName user >>= setUserID . userID
+
+main :: IO ()
+main = do
+    opts <- execParser parser
+
+    let certfiles = _ssl opts
+    certs <- mapM (readCert.snd) certfiles
+
+    let isSSL = not (null certfiles)
+        (primaryHost, primaryCert) = head certfiles
+        otherCerts = tail $ zip (map fst certfiles) certs
+
+        settings = setNoParsePath True $
+                   setServerName "Apache" $
+                   fromMaybe id (setBeforeMainLoop . setuid <$> _user opts)
+                   defaultSettings
+
+        tlsset' = tlsSettings (certfile primaryCert) (keyfile primaryCert)
+        hooks = (tlsServerHooks tlsset') { onServerNameIndication = onSNI }
+        tlsset = tlsset' { tlsServerHooks = hooks, onInsecure = AllowInsecure }
+
+        failSNI = fail "SNI" >> return mempty
+        onSNI Nothing = failSNI
+        onSNI (Just host)
+          | host == primaryHost = return mempty
+          | otherwise           = case lookup host otherCerts of
+              Nothing   -> failSNI
+              Just cert -> return (TLS.Credentials [cert])
+
+        runner | isSSL     = runTLS tlsset
+               | otherwise = runSettings
+        runnerSocket | isSSL     = runTLSSocket tlsset
+                     | otherwise = runSettingsSocket
+
+    pauth <- case _auth opts of
+        Nothing -> return Nothing
+        Just f  -> Just . flip elem . filter (isJust . BS8.elemIndex ':') . BS8.lines <$> BS8.readFile f
+    manager <- HC.newManager HC.defaultManagerSettings
+
+    let pset = ProxySettings pauth Nothing
+        proxy = (if isSSL then forceSSL else id) $ gzip def $ httpProxy pset manager dumbApp
+        port = _port opts
+
+    case _bind opts of
+        Just hostp -> runner (setHost hostp $ setPort port settings) proxy
+        Nothing    -> do
+            socket <- listenOn (PortNumber $ fromIntegral port)
+            runnerSocket settings socket proxy
