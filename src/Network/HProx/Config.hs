6a78ec719a162aaed5e8ac9bf18a19d6e07fd73e
Bin Jin <bjin@protonmail.com>
2026-05-24 12:06:00 +0800

    refactor(network-hprox): Extracted CLI config definitions into Network.HProx.Config
    - Moved `Config`, `CertFile`, `defaultConfig`, parser helpers, and `getConfig` into `Network.HProx.Config`.
    - Re-exported those symbols from `Network.HProx` so the public facade API stayed unchanged.
    - Added `Network.HProx.Config` and related modules to `library.other-modules` in `package.yaml`.
    - Kept parser option names, defaults, metavar strings, CPP-guarded fields, and version output behavior intact.
    - Confirmed formatting, tests, and Stack build checks for normal and QUIC builds were passing.

---
+++ src/Network/HProx/Config.hs
@@ -0,0 +1,207 @@
+-- SPDX-License-Identifier: Apache-2.0
+
+-- Copyright (C) 2023 Bin Jin. All Rights Reserved.
+{-# LANGUAGE CPP #-}
+
+module Network.HProx.Config
+  ( CertFile(..)
+  , Config(..)
+  , defaultConfig
+  , getConfig
+  ) where
+
+import Data.ByteString.Char8 qualified as BS8
+import Data.List             (elemIndex, elemIndices)
+import Data.List.NonEmpty    (NonEmpty(..))
+import Data.Version          (showVersion)
+import Options.Applicative
+
+import Network.HProx.Log
+import Network.HProx.Util
+import Paths_hprox
+
+-- | Configuration of HProx, see @hprox --help@ for details
+data Config = Config
+  { _bind     :: !(Maybe String)
+  , _port     :: !Int
+  , _ssl      :: ![(String, CertFile)]
+  , _auth     :: !(Maybe FilePath)
+  , _ws       :: !(Maybe BS8.ByteString)
+  , _rev      :: ![(Maybe BS8.ByteString, BS8.ByteString, BS8.ByteString)]
+  , _doh      :: !(Maybe String)
+  , _hide     :: !Bool
+  , _naive    :: !Bool
+  , _name     :: !BS8.ByteString
+  , _acme     :: !(Maybe BS8.ByteString)
+  , _log      :: !String
+  , _loglevel :: !LogLevel
+#ifdef OS_UNIX
+  , _user     :: !(Maybe String)
+  , _group    :: !(Maybe String)
+#endif
+#ifdef QUIC_ENABLED
+  , _quic     :: !(Maybe Int)
+#endif
+  }
+
+-- | Default value of 'Config', same as running @hprox@ without arguments
+defaultConfig :: Config
+defaultConfig = Config Nothing 3000 [] Nothing Nothing [] Nothing False False "hprox" Nothing "stdout" INFO
+#ifdef OS_UNIX
+  Nothing
+  Nothing
+#endif
+#ifdef QUIC_ENABLED
+    Nothing
+#endif
+
+-- | Certificate file pairs
+data CertFile = CertFile
+  { certfile :: !FilePath
+  , keyfile  :: !FilePath
+  }
+
+parser :: ParserInfo Config
+parser = info (helper <*> ver <*> config) (fullDesc <> progDesc desc)
+  where
+    parseSSL s = case splitBy ':' s of
+        host :| [cert, key] -> Right (host, CertFile cert key)
+        _otherwise          -> Left "invalid format for ssl certificates"
+
+    parseRev0 s@('/':_) = case elemIndices '/' s of
+        []      -> Nothing
+        indices -> let (prefix, remote) = splitAt (last indices + 1) s
+                   in Just (Nothing, BS8.pack prefix, BS8.pack remote)
+    parseRev0 remote = Just (Nothing, "/", BS8.pack remote)
+
+    parseRev ('/':'/':s) = case elemIndex '/' s of
+        Nothing  -> Nothing
+        Just ind -> let (domain, other) = splitAt ind s
+                    in do (_, prefix, remote) <- parseRev0 other
+                          return (Just (BS8.pack domain), prefix, remote)
+
+    parseRev s = parseRev0 s
+
+    desc = "a lightweight HTTP proxy server, and more"
+    ver = infoOption (showVersion version) (long "version" <> help "Display the version information")
+
+    config = Config <$> bind
+                    <*> port
+                    <*> ssl
+                    <*> auth
+                    <*> ws
+                    <*> rev
+                    <*> doh
+                    <*> hide
+                    <*> naive
+                    <*> name
+                    <*> acme
+                    <*> logging
+                    <*> loglevel
+#ifdef OS_UNIX
+                    <*> user
+                    <*> group
+#endif
+#ifdef QUIC_ENABLED
+                    <*> quic
+#endif
+
+    bind = optional $ strOption
+        ( long "bind"
+       <> short 'b'
+       <> metavar "bind_ip"
+       <> help "Specify the IP address to bind to (default: all interfaces)")
+
+    port = option auto
+        ( long "port"
+       <> short 'p'
+       <> metavar "port"
+       <> value 3000
+       <> showDefault
+       <> help "Specify the port number")
+
+    ssl = many $ option (eitherReader parseSSL)
+        ( long "tls"
+       <> short 's'
+       <> metavar "hostname:cerfile:keyfile"
+       <> help "Enable TLS and specify a domain with its associated TLS certificate (can be specified multiple times for multiple domains)")
+
+    auth = optional $ strOption
+        ( long "auth"
+       <> short 'a'
+       <> metavar "userpass.txt"
+       <> help "Specify the password file for proxy authentication. Plaintext passwords should be in the format 'user:pass' and will be automatically Argon2-hashed by hprox. Ensure that the password file with plaintext password is writable")
+
+    ws = optional $ strOption
+        ( long "ws"
+       <> metavar "remote-host:port"
+       <> help "Specify the remote host to handle WebSocket requests (port 443 indicates an HTTPS remote server)")
+
+    rev = many $ option (maybeReader parseRev)
+        ( long "rev"
+       <> metavar "[//domain/][/prefix/]remote-host:port"
+       <> help "Specify the remote host for reverse proxy (port 443 indicates an HTTPS remote server). An optional '//domain/' will only process requests with the 'Host: domain' header, and an optional '/prefix/' can be specified as a prefix to be matched (and stripped in proxied request)")
+
+    doh = optional $ strOption
+        ( long "doh"
+       <> metavar "dns-server:port"
+       <> help "Enable DNS-over-HTTPS (DoH) support (port 53 will be used if not specified)")
+
+    hide = switch
+        ( long "hide"
+       <> help "Never send 'Proxy Authentication Required' response. Note that this might break the use of HTTPS proxy in browsers")
+
+    naive = switch
+        ( long "naive"
+       <> help "Add naiveproxy-compatible padding (requires TLS)")
+
+    name = strOption
+        ( long "name"
+       <> metavar "server-name"
+       <> value "hprox"
+       <> showDefault
+       <> help "Specify the server name for the 'Server' header")
+
+    acme = optional $ strOption
+        ( long "acme"
+       <> metavar "ACCOUNT_THUMBPRINT"
+       <> help "Set the thumbprint for stateless http-01 ACME challenge as specified by RFC8555")
+
+    logging = strOption
+        ( long "log"
+       <> metavar "<none|stdout|stderr|file>"
+       <> value "stdout"
+       <> showDefault
+       <> help "Specify the logging type")
+
+    loglevel = option (maybeReader logLevelReader)
+        ( long "loglevel"
+       <> metavar "<trace|debug|info|warn|error|none>"
+       <> value INFO
+       <> help "Specify the logging level (default: info)")
+
+#ifdef OS_UNIX
+    user = optional $ strOption
+        ( long "user"
+       <> short 'u'
+       <> metavar "nobody"
+       <> help "Drop root priviledge and setuid to the specified user (like nobody)")
+
+    group = optional $ strOption
+        ( long "group"
+       <> short 'g'
+       <> metavar "nogroup"
+       <> help "Drop root priviledge and setgid to the specified group")
+#endif
+
+#ifdef QUIC_ENABLED
+    quic = optional $ option auto
+        ( long "quic"
+       <> short 'q'
+       <> metavar "port"
+       <> help "Enable QUIC (HTTP/3) on UDP port")
+#endif
+
+-- | Read 'Config' from command line arguments
+getConfig :: IO Config
+getConfig = execParser parser
